27b6601faa7198ced1b2ac4a588f610e4661ce69
zguo <zguo@geomar.de>
2020-02-26 18:12:31 +0100

    update mac-win

--- desktop/main.cxx
+++ desktop/main.cxx
@@ -18,33 +18,58 @@ extern int qInitResources_icons();
 
 int main( int argc, char** argv )
 {
- if(argc==2 || argc==1)
- {
-   SWEOSbash::cSWEOSarg arg;
-    if(arg.Parse(argc, argv))
-    {
-      SWEOSbash::bash_run(argc, argv);
-    }else
-    {
-      // needed to ensure appropriate OpenGL context is created for VTK rendering.
-      QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());   // must be here
-        // QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-      // QT Stuff
-      QApplication app( argc, argv );
+  #ifdef _WIN32
+      if(argc == 1)
+      {
+        // needed to ensure appropriate OpenGL context is created for VTK rendering.
+            QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());   // must be here
+              // QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+            // QT Stuff
+            QApplication app( argc, argv );
 
-      QApplication::setStyle("fusion");
+            QApplication::setStyle("fusion");
 
-      qInitResources_icons();
+            qInitResources_icons();
 
-      MainWindow myMainWindow;
-      //  myMainWindow.showMaximized();
-      myMainWindow.show();
+            MainWindow myMainWindow;
+            //  myMainWindow.showMaximized();
+            myMainWindow.show();
 
-      return app.exec();
-    }
- }else
- {
-   SWEOSbash::bash_run(argc, argv);
- }
+            return app.exec();
+      }else
+      {
+        SWEOSbash::bash_run(argc, argv);
+      }
+  #else
+      if(argc==2 || argc==1)
+      {
+        SWEOSbash::cSWEOSarg arg;
+          if(arg.Parse(argc, argv))
+          {
+            SWEOSbash::bash_run(argc, argv);
+          }else
+          {
+            // needed to ensure appropriate OpenGL context is created for VTK rendering.
+            QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());   // must be here
+              // QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+            // QT Stuff
+            QApplication app( argc, argv );
+
+            QApplication::setStyle("fusion");
+
+            qInitResources_icons();
+
+            MainWindow myMainWindow;
+            //  myMainWindow.showMaximized();
+            myMainWindow.show();
+
+            return app.exec();
+          }
+      }else
+      {
+        SWEOSbash::bash_run(argc, argv);
+      }
+  #endif
+ 
  return 0;
 }
