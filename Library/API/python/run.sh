307107289365c32daaa4b70ae1e6a6a1b223d4f2
zguo <zguo@geomar.de>
2021-03-13 10:10:02 +0100

    update python API pypi distribution

--- Library/API/python/run.sh
+++ Library/API/python/run.sh
@@ -1,4 +1,7 @@
 
 rm -rf dist
-python setup.py sdist bdist_wheel
-twine upload dist/*.whl --verbose
+
+python setup_mac.py sdist bdist_wheel --plat-name macosx-10.8-x86_64
+python setup_win32.py bdist_wheel --plat-name win32
+python setup_linux.py sdist bdist_wheel --plat-name manylinux2010_x86_64
+# twine upload dist/*.whl --verbose
