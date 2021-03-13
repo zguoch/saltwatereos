8b5d93724e6eb9b9c2e3e9d9b20aa8d345564691
zguo <zguo@geomar.de>
2021-03-13 11:42:56 +0100

    update python API pypi distribution

--- Library/API/python/run.sh
+++ Library/API/python/run.sh
@@ -1,7 +1,18 @@
-
-rm -rf dist
-
-python setup_mac.py sdist bdist_wheel --plat-name macosx-10.8-x86_64
-python setup_win32.py bdist_wheel --plat-name win32
-python setup_linux.py sdist bdist_wheel --plat-name manylinux2010_x86_64
-# twine upload dist/*.whl --verbose
+for platform in mac:macosx-10.9-x86_64 linux:manylinux2010_x86_64 win32:win32
+do
+    platName=$(echo $platform | cut -d ":" -f 1)
+    platTag=$(echo $platform | cut -d ":" -f 2)
+    # create __init__.py in module folder !!!important
+	touch ${platName}/pyswEOS/__init__.py
+    # include libraries as materials
+    cp MANIFEST.in ${platName} 
+    # cp versions and setup.py
+    cp version.txt ${platName} 
+    cp setup.py ${platName} 
+    # generate wheel binary package
+    cd ${platName} 
+    version=`cat version.txt`
+    python setup.py sdist bdist_wheel --plat-name $platTag
+    twine upload dist/*${version}*.whl --verbose
+    cd ..
+done
---
+++ Library/API/python/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+version=1.0
+try:
+    with open('version.txt') as f:
+        lines = f.readlines()
+        version=lines[0]
+except:
+    print('Please provide version file in **/API/python folder')
+    exit(0)
+    pass
+setup(
+    name='pyswEOS',
+    version=version,
+    author='Zhikui Guo',
+    author_email='zguo@geomar.de',
+    url='https://www.scibyte.cn',
+    description=u'Python API of swEOS library which is designed to calculate EOS and thermal dynamic properties of H2O-NaCl system, usually called salt water.',
+    packages=find_packages(where='.', exclude=('docs_pyH2ONaCl'), include=('*',)), 
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'argparse',
+        'colored'
+        ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': []
+    },
+    keywords = "NaCl H2O IAPWS Hydrothermal seawater"
+)
