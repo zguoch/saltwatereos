a71e81225157cc131a07fa8bc7c6aece316f4b60
Guo zhikui <zhikuiguo@live.cn>
2021-03-16 03:52:55 +0800

    finished CPACK on windows

--- commandline/swEOSVersion.h
+++ commandline/swEOSVersion.h
@@ -14,14 +14,14 @@
 #define SWEOS_MAJOR_VERSION 1
 #define SWEOS_MINOR_VERSION 7
 #define SWEOS_PATCH_VERSION 0
-#define SWEOS_EXTRA_VERSION "-git-6373429"
-#define SWEOS_VERSION       "1.7.0-git-6373429"
+#define SWEOS_EXTRA_VERSION "-git-012f51f"
+#define SWEOS_VERSION       "1.7.0-git-012f51f"
 #define SWEOS_DATE          "2021-03-16"
 #define SWEOS_HOST          "DESKTOP-QGQV2GR"
 #define SWEOS_PACKAGER      "desktop-qgqv2gr zhiku"
-#define SWEOS_OS            "Windows"
+#define SWEOS_OS            ""
 #define SWEOS_SHORT_LICENSE "GNU General Public License"
-#define SWEOS_QT_VERSION      "5"
-#define SWEOS_VTK_VERSION       "8.2.0"
+#define SWEOS_QT_VERSION      ""
+#define SWEOS_VTK_VERSION       ""
 
 #endif
