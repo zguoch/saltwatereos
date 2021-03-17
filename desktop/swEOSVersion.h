a907910e3a9c3a7acd2d908e5d70a4f73126c15c
Guo zhikui <zhikuiguo@live.cn>
2021-03-18 03:02:01 +0800

    update windows version

--- desktop/swEOSVersion.h
+++ desktop/swEOSVersion.h
@@ -14,12 +14,12 @@
 #define SWEOS_MAJOR_VERSION 1
 #define SWEOS_MINOR_VERSION 7
 #define SWEOS_PATCH_VERSION 0
-#define SWEOS_EXTRA_VERSION "-git-02b793a"
-#define SWEOS_VERSION       "1.7.0-git-02b793a"
-#define SWEOS_DATE          "2021-03-17"
-#define SWEOS_HOST          "zhikuis-MacBook-Pro.local"
-#define SWEOS_PACKAGER      "zguo"
-#define SWEOS_OS            "MacOSX"
+#define SWEOS_EXTRA_VERSION "-git-efe7a26"
+#define SWEOS_VERSION       "1.7.0-git-efe7a26"
+#define SWEOS_DATE          "2021-03-18"
+#define SWEOS_HOST          "DESKTOP-QGQV2GR"
+#define SWEOS_PACKAGER      "desktop-qgqv2gr zhiku"
+#define SWEOS_OS            "Windows"
 #define SWEOS_SHORT_LICENSE "GNU General Public License"
 #define SWEOS_QT_VERSION      "5"
 #define SWEOS_VTK_VERSION       "8.2.0"
