660fe8b920df962379ef982a02d7fb6bf785bafe
zguo <zguo@geomar.de>
2021-03-24 12:12:44 +0100

    update run.sh in Python API

--- Library/API/python/run.sh
+++ Library/API/python/run.sh
@@ -1,20 +1,43 @@
 
 # 拷贝不同系统的Release中的API文件夹到当前目录，分别命名为mac, linux, win
-for platform in mac:macosx-10.9-x86_64
-do
-    platName=$(echo $platform | cut -d ":" -f 1)
-    platTag=$(echo $platform | cut -d ":" -f 2)
-    # create __init__.py in module folder !!!important
-	touch ${platName}/pyswEOS/__init__.py
-    # include libraries as materials
-    cp MANIFEST.in ${platName} 
-    # cp versions and setup.py
-    cp version.txt ${platName} 
-    cp setup.py ${platName} 
-    # generate wheel binary package
-    cd ${platName} 
-    version=`cat version.txt`
-    python setup.py sdist bdist_wheel --plat-name $platTag
-    twine upload dist/*.whl --verbose
-    cd ..
-done
+function distPython()
+{
+    platTag=$1
+    pythonTags=$2
+    packageName=pyswEOS
+    for pythonTag in $pythonTags
+    do
+        # pythonTag=$(echo $platform | cut -d ":" -f 1)
+        # platTag=$(echo $platform | cut -d ":" -f 2)
+
+        majorTag=$(echo $pythonTag | cut -d "." -f 1)
+        minorTag=$(echo $pythonTag | cut -d "." -f 2)
+        # pyTag=${majorTag}${minorTag}
+        pyTag=py$pythonTag
+        echo $pythonTag $pyTag
+        pkgFolder=pyswEOS_python$pythonTag
+        cp -rf $pkgFolder $packageName
+
+        # create __init__.py in module folder !!!important
+        touch ${packageName}/__init__.py
+
+        # include libraries as materials
+        cp MANIFEST.in ${packageName} 
+
+        # change python tag in setup.cfg
+        awk -v pythonTag=$pyTag '{gsub(/python-tag = pythonTag/, "python-tag = "pythonTag""); print }' setup.cfg.in > setup.cfg
+
+        # build
+        python setup.py bdist_wheel --plat-name $platTag
+
+        version=`cat version.txt`
+        # upload to PyPi
+        whlFile=dist/pyswEOS-$version-$pyTag-none-$platTag.whl
+        ls $whlFile
+        twine upload $whlFile --verbose
+
+        rm -rf $packageName
+    done
+}
+
+distPython manylinux2010_x86_64 "2.7 3.5m 3.6m 3.7m 3.8 3.9"
--- Library/API/python/setup.py
+++ Library/API/python/setup.py
@@ -16,8 +16,7 @@ setup(
     version=version,
     author='Zhikui Guo',
     author_email='zguo@geomar.de',
-    url='https://www.scibyte.cn',
-    python_requires='==3.8',
+    url='https://www.sweos.info',
     description=u'Python API of swEOS library which is designed to calculate EOS and thermal dynamic properties of H2O-NaCl system, usually called salt water.',
     packages=find_packages(where='.', exclude=('docs_pyH2ONaCl'), include=('*',)), 
     install_requires=[
