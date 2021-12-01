716eb7ddaf39145c362ee0e8c51f4adb41cc33f5
zguo <zguo@geomar.de>
2021-12-01 23:21:58 +0100

    update

--- test/p4est/QuadTree.cpp
+++ test/p4est/QuadTree.cpp
@@ -8,10 +8,12 @@ m_num_children(1<<dim),
 m_data_size(data_size),
 m_max_level(max_level)
 {
+    double length_forest = (1<<MAX_FOREST_LEVEL);
     for (size_t i = 0; i < 3; i++)
     {
         m_xyz_max[i] = xyz_max[i];
         m_xyz_min[i] = xyz_min[i];
+        length_scale[i] = (m_xyz_max[i] - m_xyz_min[i])/length_forest;
     }
     
     init_Root(m_root);
@@ -20,6 +22,29 @@ template <int dim, typename USER_DATA>
 cForest<dim,USER_DATA>::~cForest()
 {
     release_quadrant_data(&m_root);
+    release_children(&m_root);
+}
+
+template <int dim, typename USER_DATA> 
+void cForest<dim,USER_DATA>::release_children(Quadrant<dim,USER_DATA>* quad)
+{
+    if(quad->isHasChildren)
+    {
+        for (int i = 0; i < m_num_children; i++)
+        {
+            if(quad->children[i]->isHasChildren)
+            {
+                release_children(quad->children[i]);
+            }else
+            {
+                delete quad->children[i];
+            }
+        }
+        
+    }else
+    {
+        return;
+    }
 }
 
 template <int dim, typename USER_DATA> 
@@ -40,75 +65,79 @@ void cForest<dim,USER_DATA>::release_quadrant_data(Quadrant<dim,USER_DATA>* quad
 template <int dim, typename USER_DATA> 
 void cForest<dim,USER_DATA>::init_Root(Quadrant<dim,USER_DATA>& quad)
 {
-    quad.xyz[0] = 0;
-    quad.xyz[1] = 0;
-    quad.xyz[2] = 0;
+    quad.xyz[0] = m_xyz_min[0];
+    quad.xyz[1] = m_xyz_min[1];
+    quad.xyz[2] = m_xyz_min[2];
     quad.level  = 0;
     quad.isHasChildren = false;
     if(m_data_size!=0) quad.user_data = new USER_DATA;  //only allocate memory if it is a leaf, this will be released when a quadrent is refined.
 }
 
 template <int dim, typename USER_DATA>
-void cForest<dim,USER_DATA>::refine_uniform(Quadrant<dim,USER_DATA>* quad, int max_level_refine)
+void cForest<dim,USER_DATA>::refine(Quadrant<dim,USER_DATA>* quad, bool (*is_refine)(cForest<dim,USER_DATA>* forest, Quadrant<dim,USER_DATA>* quad, int max_level))
 {
-    if(quad->level >= max_level_refine)return;
+    if(quad->level >= m_max_level)return;
 
-    // if no children, create children
-    if(!quad->isHasChildren)
+    if(is_refine(this, quad, m_max_level))
     {
-        // make children and release user_data, because the non-leaf quad doesn't need user data
-        int length_child = 1<<(m_max_level - quad->level -1);
-        // first child: lower left
-        quad->children[0] = new Quadrant<dim,USER_DATA>;
-        quad->children[0]->xyz[0] = quad->xyz[0];
-        quad->children[0]->xyz[1] = quad->xyz[1];
-        quad->children[0]->xyz[2] = quad->xyz[2];
-        quad->children[0]->level = quad->level+1;
-        quad->children[0]->isHasChildren = false;
-        if(m_data_size!=0) quad->children[0]->user_data = new USER_DATA; 
-        // second child: lower right
-        quad->children[1] = new Quadrant<dim,USER_DATA>;
-        quad->children[1]->xyz[0] = quad->xyz[0] + length_child;
-        quad->children[1]->xyz[1] = quad->xyz[1];
-        quad->children[1]->xyz[2] = quad->xyz[2];
-        quad->children[1]->level = quad->level+1;
-        quad->children[1]->isHasChildren = false;
-        if(m_data_size!=0) quad->children[1]->user_data = new USER_DATA; 
-        // third child: upper left
-        quad->children[2] = new Quadrant<dim,USER_DATA>;
-        quad->children[2]->xyz[0] = quad->xyz[0];
-        quad->children[2]->xyz[1] = quad->xyz[1] + length_child;
-        quad->children[2]->xyz[2] = quad->xyz[2];
-        quad->children[2]->level = quad->level+1;
-        quad->children[2]->isHasChildren = false;
-        if(m_data_size!=0) quad->children[2]->user_data = new USER_DATA; 
-        // second child: upper right
-        quad->children[3] = new Quadrant<dim,USER_DATA>;
-        quad->children[3]->xyz[0] = quad->xyz[0] + length_child;
-        quad->children[3]->xyz[1] = quad->xyz[1] + length_child;
-        quad->children[3]->xyz[2] = quad->xyz[2];
-        quad->children[3]->level = quad->level+1;
-        quad->children[3]->isHasChildren = false;
-        if(m_data_size!=0) quad->children[3]->user_data = new USER_DATA; 
-        //  if dim==3
-        //\todo 补充三维的情况
+        // if no children, create children
+        if(!quad->isHasChildren)
+        {
+            // make children and release user_data, because the non-leaf quad doesn't need user data
+            int length_child = 1<<(MAX_FOREST_LEVEL - quad->level -1); //Note that must -1, because this is the child length
+            // first child: lower left
+            quad->children[0] = new Quadrant<dim,USER_DATA>;
+            quad->children[0]->xyz[0] = quad->xyz[0];
+            quad->children[0]->xyz[1] = quad->xyz[1];
+            quad->children[0]->xyz[2] = quad->xyz[2];
+            quad->children[0]->level = quad->level+1;
+            quad->children[0]->isHasChildren = false;
+            if(m_data_size!=0) quad->children[0]->user_data = new USER_DATA; 
+            // second child: lower right
+            quad->children[1] = new Quadrant<dim,USER_DATA>;
+            quad->children[1]->xyz[0] = quad->xyz[0] + length_child*length_scale[0];
+            quad->children[1]->xyz[1] = quad->xyz[1];
+            quad->children[1]->xyz[2] = quad->xyz[2];
+            quad->children[1]->level = quad->level+1;
+            quad->children[1]->isHasChildren = false;
+            if(m_data_size!=0) quad->children[1]->user_data = new USER_DATA; 
+            // third child: upper left
+            quad->children[2] = new Quadrant<dim,USER_DATA>;
+            quad->children[2]->xyz[0] = quad->xyz[0];
+            quad->children[2]->xyz[1] = quad->xyz[1] + length_child*length_scale[1];
+            quad->children[2]->xyz[2] = quad->xyz[2];
+            quad->children[2]->level = quad->level+1;
+            quad->children[2]->isHasChildren = false;
+            if(m_data_size!=0) quad->children[2]->user_data = new USER_DATA; 
+            // second child: upper right
+            quad->children[3] = new Quadrant<dim,USER_DATA>;
+            quad->children[3]->xyz[0] = quad->xyz[0] + length_child*length_scale[0];
+            quad->children[3]->xyz[1] = quad->xyz[1] + length_child*length_scale[1];
+            quad->children[3]->xyz[2] = quad->xyz[2];
+            quad->children[3]->level = quad->level+1;
+            quad->children[3]->isHasChildren = false;
+            if(m_data_size!=0) quad->children[3]->user_data = new USER_DATA; 
+            //  if dim==3
+            //\todo 补充三维的情况
 
-        // delete parent data
-        delete quad->user_data;
-        quad->user_data = NULL;
-        quad->isHasChildren = true;
-    }
+            // delete parent data
+            delete quad->user_data;
+            quad->user_data = NULL;
+            quad->isHasChildren = true;
+        }
 
-    for (size_t i = 0; i < m_num_children; i++)
-    {
-        refine_uniform(quad->children[i], max_level_refine);
+        for (size_t i = 0; i < m_num_children; i++)
+        {
+            refine(quad->children[i], is_refine);
+        }
     }
+    
 }
 
 template <int dim, typename USER_DATA>
-void cForest<dim,USER_DATA>::refine_uniform(int max_level_refine)
+void cForest<dim,USER_DATA>::refine(bool (*is_refine)(cForest<dim,USER_DATA>* forest, Quadrant<dim,USER_DATA>* quad, int max_level))
 {
-    refine_uniform(&m_root, max_level_refine);
+    refine(&m_root, is_refine);
 }
 
 template <int dim, typename USER_DATA>
@@ -136,18 +165,13 @@ void cForest<dim,USER_DATA>::write_to_vtk(const Quadrant<dim,USER_DATA>* quad)
 template <int dim, typename USER_DATA>
 void cForest<dim,USER_DATA>::write_to_vtk(string filename)
 {
-    double length_forest = 1<<m_max_level; //reference system is a square
-    double length_scale[3]; //map real xyz to reference system
-    double length_cell[3];
-    for (int i = 0; i < 3; i++)
-    {
-        length_scale[i] = (m_xyz_max[i] - m_xyz_min[i])/length_forest;
-    }
     vector<Quadrant<dim,USER_DATA>* > leaves;
     getLeaves(leaves, &m_root);
     int num_points_per_cell = 1<<dim;
     int num_cells = leaves.size();
     int num_points = num_cells*num_points_per_cell;
+    double length_cell[3]; //physical length
+    double length_ref_cell = 0;
 
     ofstream fout(filename);
     // write head
@@ -163,20 +187,19 @@ void cForest<dim,USER_DATA>::write_to_vtk(string filename)
     // write points
     fout<<"      <Points>"<<endl;
     fout<<"        <DataArray type=\"Float32\" Name=\"Position\" NumberOfComponents=\"3\" format=\"ascii\" RangeMin=\"0.008838834896540746\" RangeMax=\"1.4053746938907843\">"<<endl;
-    double length_ref_cell = 0;
-    double xyz_cell[3]; //lower left corner
+    
     for (size_t i = 0; i < num_cells; i++)
     {
-        length_ref_cell = 1<<(m_max_level - leaves[i]->level);
+        length_ref_cell = 1<<(MAX_FOREST_LEVEL - leaves[i]->level);
         for (int j = 0; j < 3; j++)
         {
             length_cell[j] = length_scale[j] * length_ref_cell;
-            xyz_cell[j] = leaves[i]->xyz[j]*length_scale[j] + m_xyz_min[j];
         }
-        fout<<"         "<<xyz_cell[0]<<" "<<xyz_cell[1]<<" "<<xyz_cell[2]; //Lower left
-        fout<<" "<<xyz_cell[0] + length_cell[0]<<" "<<xyz_cell[1]<<" "<<xyz_cell[2]; //lower right
-        fout<<" "<<xyz_cell[0]<<" "<<xyz_cell[1] + length_cell[1]<<" "<<xyz_cell[2]; //upper left
-        fout<<" "<<xyz_cell[0] + length_cell[0]<<" "<<xyz_cell[1] + length_cell[1]<<" "<<xyz_cell[2]; //upper right
+        fout<<"        ";
+        fout<<" "<<leaves[i]->xyz[0]                 <<" "<<leaves[i]->xyz[1]                <<" "<<leaves[i]->xyz[2]; //Lower left
+        fout<<" "<<leaves[i]->xyz[0] + length_cell[0]<<" "<<leaves[i]->xyz[1]                <<" "<<leaves[i]->xyz[2]; //lower right
+        fout<<" "<<leaves[i]->xyz[0]                 <<" "<<leaves[i]->xyz[1] + length_cell[1]<<" "<<leaves[i]->xyz[2]; //upper left
+        fout<<" "<<leaves[i]->xyz[0] + length_cell[0]<<" "<<leaves[i]->xyz[1] + length_cell[1]<<" "<<leaves[i]->xyz[2]; //upper right
         fout<<endl;
     }
     fout<<"        </DataArray>"<<endl;
@@ -215,17 +238,97 @@ void cForest<dim,USER_DATA>::write_to_vtk(string filename)
     fout.close();
 }
 
+template <int dim, typename USER_DATA>
+void cForest<dim,USER_DATA>::get_quadrant_physical_length(int level, double physical_length[3])
+{
+    int length_ref = 1<<(MAX_FOREST_LEVEL - level);
+    for (int i = 0; i < 3; i++)
+    {
+        physical_length[i] = length_scale[i] * length_ref;
+    }
+}
+
+int phaseRegion_sin_func(double x, double y)
+{
+    double value = sin(x);
+    int region = 0;
+    if(value==y)
+    {
+        region = 0;
+    }else if(y > value)
+    {
+        region = 1;
+    }else
+    {
+        region = -1;
+    }
+    return region;
+}
+
+template <int dim, typename USER_DATA>
+bool refine_fn(cForest<dim,USER_DATA>* forest, Quadrant<dim,USER_DATA>* quad, int max_level)
+{
+    // FIELD_DATA       *data = (FIELD_DATA *) q->p.user_data;
+    // p4est_qcoord_t      half_length = P4EST_QUADRANT_LEN (q->level) / 2;
+    // p4est_qcoord_t      length = P4EST_QUADRANT_LEN (q->level);
+    bool need_refine = false;
+    double physical_length[3];
+    forest->get_quadrant_physical_length(quad->level, physical_length);
+    const int num_sample_x =2; //最简单的情况就是只取xmin, xmax作为采样点判断这些采样点的函数计算返回值(flat)是否全部相等.但是有时候会有漏掉的情况，所以可以考虑在这里加密采样
+    const int num_sample_y =2;
+    double dx_qua = physical_length[0] / (num_sample_x - 1.0);
+    double dy_qua = physical_length[0] / (num_sample_y - 1.0);
+    double x_qua, x_ref, y_qua, y_ref;
+    double xyz_tmp[3];
+    int regionIndex[num_sample_x*num_sample_y];
+    for (int iy = 0; iy < num_sample_y; iy++)
+    {
+        y_qua = quad->xyz[1] + dy_qua*iy;
+        for (int ix = 0; ix < num_sample_x; ix++)
+        {
+            x_qua = quad->xyz[0] + dx_qua*ix;
+            regionIndex[iy*num_sample_x + ix] = phaseRegion_sin_func(x_qua, y_qua); //phaseRegion_H2ONaCl_constantX(xyz_tmp[0], xyz_tmp[1]);
+        }
+    }
+    // ========== 1. refinement check for phase index ============
+    bool isSame_phaseIndex = true;
+    for (int i = 1; i < num_sample_x*num_sample_y; i++)
+    {
+        isSame_phaseIndex = (isSame_phaseIndex && (regionIndex[0] == regionIndex[i]));
+    }
+    if(!isSame_phaseIndex) //如果采样点的phase index全相等，则不refine；否则refine
+    {
+        need_refine = true;
+    }else
+    {
+        need_refine = false;
+    }
+
+    if(quad->level >= forest->m_max_level)return false;
+    if(need_refine)return true;
+
+    return false;
+}
+
+template <int dim, typename USER_DATA>
+bool refine_latest(cForest<dim,USER_DATA>* forest, Quadrant<dim,USER_DATA>* quad, int max_level)
+{
+    if(quad->level <= 4)return true;
+
+    return false;
+}
 int main()
 {
-    double xyzmin[3] = {0,0,0};
-    double xyzmax[3] = {700, 400, 100};
+    double xyzmin[3] = {0,-2,0};
+    double xyzmax[3] = {2*3.141592653, 1.5, 100};
     typedef double type_user_data;
-    int max_level = 5;
+    int max_level = 12;
     cForest<2, type_user_data> forest(xyzmin, xyzmax, max_level, sizeof(type_user_data));
 
     // refine 
-    forest.refine_uniform(4);
-
+    // forest.refine_uniform(4);
+    forest.refine(refine_latest);
+    forest.refine(refine_fn);
     forest.write_to_vtk("quadTree.vtu");
 
     return 0;
--- test/p4est/QuadTree.h
+++ test/p4est/QuadTree.h
@@ -6,15 +6,20 @@
 using namespace std;
 #include <cmath>
 
+#define MAX_FOREST_LEVEL 29
+
 template <int dim, typename USER_DATA>
 struct Quadrant
 {
-    int         xyz[3]; 
+    double         xyz[3]; //real coordinate of the lower left corner of a quadrant
     int         level;
     bool        isHasChildren;
     Quadrant    *children[1<<dim]; //2^dim
     USER_DATA        *user_data;
 };
+
+
+// typedef template <int dim, typename USER_DATA> bool (*refine_func)(Quadrant<dim,USER_DATA>* quad);
 
 /**
  * @brief Pass dimension and data type to the class
@@ -28,17 +33,21 @@ class cForest
 private:
     int     m_num_children;
     size_t  m_data_size;
-    int     m_max_level;
     double m_xyz_min[3];
     double m_xyz_max[3];
+    double length_scale[3]; /**< The reference space is a square or a cube with length=2^{MAX_FOREST_LEVEL}, so the length scale in x,y,z axis is calculated as, e.g. length_scale[0] = (m_xyz_max[0] - m_xyz_min[0])/length, so the length of a quadrant is len_quad = 2^{MAX_FOREST_LEVEL - level}, so its real length in x-axis is len_quad*length_scale[0] */
     Quadrant<dim,USER_DATA> m_root;
     void init_Root(Quadrant<dim,USER_DATA>& quad);
     void release_quadrant_data(Quadrant<dim,USER_DATA>* quad);
+    void release_children(Quadrant<dim,USER_DATA>* quad);
     void write_to_vtk(const Quadrant<dim,USER_DATA>* quad);
     void getLeaves(vector<Quadrant<dim,USER_DATA>* >& leaves, Quadrant<dim,USER_DATA>* quad);
-    void refine_uniform(Quadrant<dim,USER_DATA>* quad, int max_level);
+    void refine(Quadrant<dim,USER_DATA>* quad, bool (*is_refine)(cForest<dim,USER_DATA>* forest, Quadrant<dim,USER_DATA>* quad, int max_level));
+    
 public:
-    void refine_uniform(int max_level);
+    int     m_max_level;
+    void get_quadrant_physical_length(int level, double physical_length[3]);
+    void refine(bool (*is_refine)(cForest<dim,USER_DATA>* forest, Quadrant<dim,USER_DATA>* quad, int max_level));
     void write_to_vtk(string filename);
     cForest(double xyz_min[3], double xyz_max[3], int max_level, size_t data_size);
     ~cForest();
