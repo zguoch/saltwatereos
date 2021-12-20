45bab781a9d0e54e1d596e8de8106a19adee3c3d
zguo <zguo@geomar.de>
2021-12-20 11:28:13 +0100

    update

--- Library/Matlab/H2ONaCl/lutLookup_2D.cpp
+++ Library/Matlab/H2ONaCl/lutLookup_2D.cpp
@@ -52,6 +52,109 @@ void mGetMatrix(const mxArray *prhs, double **out, const char *varname, mwSize *
   *out_m = m;
   *out_n = n;
 }
+void load_binary_2d(string filename)
+{
+    int ind = 0;
+    H2ONaCl::cH2ONaCl eos;
+    H2ONaCl::PROP_H2ONaCl prop_cal;
+    clock_t start = clock();
+    const int dim = 2;
+    int dim_file = LOOKUPTABLE_FOREST::get_dim_from_binary(filename);
+    if(dim!=dim_file)ERROR("The input LUT file is not 2D.");
+    eos.loadLUT(filename);
+    H2ONaCl::LookUpTableForest_2D* pLUT = (H2ONaCl::LookUpTableForest_2D*)eos.m_pLUT;
+    // eos.save_lut_to_vtk("lut_load_save.vtu");
+    double Xmin = pLUT->m_xyz_min[0], Xmax = pLUT->m_xyz_max[0], Ymin = pLUT->m_xyz_min[1], Ymax = pLUT->m_xyz_max[1];
+    double constZ = pLUT->m_constZ;
+    int index_rho = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_rho));
+    int index_h = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_h));
+    int index_T = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_T));
+
+    STATUS("Start search ... ");
+    start = clock();
+    LOOKUPTABLE_FOREST::Quadrant<dim,LOOKUPTABLE_FOREST::FIELD_DATA<dim> > *targetLeaf = NULL;
+    int n_randSample = 1E3;
+    double* props = new double[pLUT->m_map_props.size()];
+    int ind_rho = 0;
+    int nx = 100, ny = 150;
+    double dx = (Xmax - Xmin)/(nx - 1);
+    double dy = (Ymax - Ymin)/(ny - 1);
+    double x, y;
+    ofstream fpout_xx("XX.txt");
+    ofstream fpout_yy("YY.txt");
+    ofstream fpout_zz("ZZ.txt");
+    ofstream fpout_zz_lut("ZZ_lut.txt");
+    bool isCal = true;
+    for (int i = 0; i < ny; i++)
+    {
+        y = Ymin + i*dy;
+        for (int j = 0; j < nx; j++)
+        {
+            x = Xmin + j*dx;
+            if(isCal)
+            {
+                switch (pLUT->m_TorH)
+                {
+                case LOOKUPTABLE_FOREST::EOS_ENERGY_T:
+                    {
+                        switch (pLUT->m_const_which_var)
+                        {
+                        case LOOKUPTABLE_FOREST::CONST_X_VAR_TorHP:
+                            prop_cal = eos.prop_pTX(y, x, constZ);
+                            break;
+                        case LOOKUPTABLE_FOREST::CONST_P_VAR_XTorH:
+                            prop_cal = eos.prop_pTX(constZ, y, x);
+                            break;
+                        case LOOKUPTABLE_FOREST::CONST_TorH_VAR_XP:
+                            prop_cal = eos.prop_pTX(y, constZ, x);
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                    break;
+                case LOOKUPTABLE_FOREST::EOS_ENERGY_H:
+                    {
+                        switch (pLUT->m_const_which_var)
+                        {
+                        case LOOKUPTABLE_FOREST::CONST_X_VAR_TorHP:
+                            prop_cal = eos.prop_pHX(y, x, constZ);
+                            break;
+                        case LOOKUPTABLE_FOREST::CONST_P_VAR_XTorH:
+                            prop_cal = eos.prop_pHX(constZ, y, x);
+                            break;
+                        case LOOKUPTABLE_FOREST::CONST_TorH_VAR_XP:
+                            prop_cal = eos.prop_pHX(y, constZ, x);
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+                }
+                fpout_zz<<prop_cal.Rho<<" ";
+            }
+            
+            targetLeaf = eos.lookup(props, x, y, false);
+            fpout_xx<<x<<" ";
+            fpout_yy<<y<<" ";
+            fpout_zz_lut<<props[index_rho]<<" ";
+        }
+        fpout_xx<<endl;
+        fpout_yy<<endl;
+        fpout_zz<<endl;
+        fpout_zz_lut<<endl;
+    }
+    fpout_xx.close();
+    fpout_yy.close();
+    fpout_zz.close();
+    fpout_zz_lut.close();
+    delete[] props;
+
+    STATUS_time("Searching done", clock() - start);
+}
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
@@ -80,32 +183,58 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     clock_t start = clock();
     STATUS("Passing data end, start loading data ...");
     H2ONaCl::cH2ONaCl sw;
-    sw.loadLUT(filename); //\Todo pass this sw pointer as input arg from outside, don't always load the LUT file.
-    STATUS_time("Load LUT end. ", clock()-start);
-    start = clock();
-    STATUS("Start looking up ...");
-    const int dim = 2;
-    H2ONaCl::LookUpTableForest_2D* pLUT = (H2ONaCl::LookUpTableForest_2D*)sw.m_pLUT;
-    LOOKUPTABLE_FOREST::Quadrant<dim,LOOKUPTABLE_FOREST::FIELD_DATA<dim> > *targetLeaf = NULL;
-    double* props = new double[pLUT->m_map_props.size()];
-    // ====== get order of props in the data array =====
-    int index_Rho = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_rho));
-    int index_T = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_T));
-    // =================================================
+    load_binary_2d(filename);
 
-    int index = 0;
-    for (int i = 0; i < m; i++)
-    {
-      for (int j = 0; j < n; j++)
-      {
-        index = i*n +j;
-        targetLeaf = sw.lookup(props, xMat[index], yMat[index]); 
-        needRefine_out[index] = targetLeaf->user_data->need_refine;
-        T_out[index] = props[index_T];
-        Rho_out[index] = props[index_Rho]; 
-      }
-    }
+    // sw.loadLUT(filename); //\Todo pass this sw pointer as input arg from outside, don't always load the LUT file.
+    // STATUS_time("Load LUT end. ", clock()-start);
+    // start = clock();
+    // STATUS("Start looking up ...");
+    // const int dim = 2;
+    // H2ONaCl::LookUpTableForest_2D* pLUT = (H2ONaCl::LookUpTableForest_2D*)sw.m_pLUT;
+    // LOOKUPTABLE_FOREST::Quadrant<dim,LOOKUPTABLE_FOREST::FIELD_DATA<dim> > *targetLeaf = NULL;
+    // double* props = new double[pLUT->m_map_props.size()];
+    // // ====== get order of props in the data array =====
+    // int index_Rho = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_rho));
+    // int index_T = distance(pLUT->m_map_props.begin(),pLUT->m_map_props.find(Update_prop_T));
+    // // =================================================
+    // // ofstream fpout_x("XX.txt");
+    // // ofstream fpout_y("YY.txt");
+    // // ofstream fpout_z("ZZ.txt");
+    // // ofstream fpout_prop0("prop0.txt");
+    // // ofstream fpout_prop1("prop1.txt");
+    // // ofstream fpout_prop2("prop2.txt");
+    // int index = 0;
+    // for (int i = 0; i < m; i++)
+    // {
+    //   for (int j = 0; j < n; j++)
+    //   {
+    //     index = i*n +j;
+    //     targetLeaf = sw.lookup(props, xMat[index], yMat[index]); 
+    //     needRefine_out[index] = targetLeaf->user_data->need_refine;
+    //     T_out[index] = props[index_T];
+    //     Rho_out[index] = props[index_Rho]; 
+    //     // fpout_x<<xMat[index]<<" ";
+    //     // fpout_y<<yMat[index]<<" ";
+    //     // fpout_z<<yMat[index]<<" ";
+    //     // fpout_prop0<<props[0]<<" ";
+    //     // fpout_prop1<<props[1]<<" ";
+    //     // fpout_prop2<<props[2]<<" ";
+    //   }
+    //   // fpout_x<<endl;
+    //   // fpout_y<<endl;
+    //   // fpout_z<<endl;
+    //   // fpout_prop0<<endl;
+    //   // fpout_prop1<<endl;
+    //   // fpout_prop2<<endl;
+    // }
+    // // fpout_x.close();
+    // // fpout_y.close();
+    // // fpout_z.close();
+    // // fpout_prop0.close();
+    // // fpout_prop1.close();
+    // // fpout_prop2.close();
 
-    delete[] props;
-    STATUS_time("Lookup end. ", clock()-start);
+
+    // delete[] props;
+    // STATUS_time("Lookup end. ", clock()-start);
 }
